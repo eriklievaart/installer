c144162a4d37feed500d8da8357ab21d69d6c558
Jantje <jantje@example.com>
2018-06-12 08:41:38 +0200

    new: download and install junit, checkstyle new: scripts eclipse, each, automount, goo, imports, quickdiff new: compile and install hobby projects cleanup: deleted scripts that are now generated

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-
-
 set -e                    # fail on errors
 sh -n install.sh          # check this file for syntax errors before executing it
 
+
 HOME=~
-LOGFILE=/tmp/installer_$(date +%y-%m-%d_%T).log
+LOG_DIR=/tmp/installer
+LOG_FILE=$LOG_DIR/installer_$(date +%y-%m-%d_%T).log
 UP_TO_DATE=false
+
 JAVA_PKG="openjdk-8-jdk"
 JAVA_VERSION="1.8"
+
+CHECKSTYLE_VERSION=6.14.1
+CHECKSTYLE_DESTINATION=~/Applications/checkstyle/checkstyle.jar
+CHECKSTYLE_CACHE="../cache/checkstyle/checkstyle-$CHECKSTYLE_VERSION.jar"
+CHECKSTYLE_URL="https://cytranet.dl.sourceforge.net/project/checkstyle/checkstyle/$CHECKSTYLE_VERSION/checkstyle-$CHECKSTYLE_VERSION-all.jar"
+
+ANT_JUNIT_VERSION=1.8.4
+ANT_JUNIT_DESTINATION=~/.ant/lib/ant-junit.jar
+ANT_JUNIT_CACHE="../cache/ant-junit/ant-junit-$ANT_JUNIT_VERSION.jar"
+ANT_JUNIT_URL="https://repo1.maven.org/maven2/org/apache/ant/ant-junit/$ANT_JUNIT_VERSION/ant-junit-$ANT_JUNIT_VERSION.jar"
 
 
 die() {
@@ -17,8 +28,12 @@ die() {
 }
 
 log() { 
+	if [ ! -d "$LOG_DIR" ]
+	then
+		mkdir $LOG_DIR
+	fi
 	echo $1
-	echo $1 >> $LOGFILE
+	echo $1 >> $LOG_FILE
 }
 
 check_status() {
@@ -31,8 +46,9 @@ check_status() {
 update_once() {
 	if [ $UP_TO_DATE ]
 	then
+		set +e
 		sudo apt-get update
-		check_status $?
+		set -e
 		UP_TO_DATE=true
 	fi
 }
@@ -49,17 +65,39 @@ install() {
 	fi
 }
 
-if [ "$(id -u)" = "0" ]; then
-	echo "usage: sh install.sh"
-	die "This script must be run as a normal user, not as root"
-fi
+fetch_url() {
+	cache_file=$1
+	url=$2
+	destination=$3
+
+	if [ -f $destination ]
+	then
+		echo "already installed: $destination"
+	else
+		sh bin/wgetc $cache_file $url $destination
+	fi
+}
 
 if [ "$HOME" = "/root" ]; then
 	echo "usage: sudo sh install.sh"
-	die "run script with root priviliges, not as root user"
+	die "run script with root priviliges (sudo sh install.sh), not as root user"
 fi
 
-sudo ufw enable # enable firewall
+if [ ! -f ~/.gitconfig ]
+then
+	echo "enter git username"
+	read git_user
+	echo "enter git email"
+	read git_email
+
+	install git
+	git config --global user.name "$git_user"
+	git config --global user.email "$git_email"
+	git config --global credential.helper cache
+fi
+
+# enable firewall
+sudo ufw enable 
 echo ""
 
 
@@ -80,22 +118,26 @@ fi
 # allow users to invoke shutdown
 sudo chmod +s /sbin/shutdown
 
-# backup original grub config (only the first time run)
-sudo cp -n /etc/default/grub /etc/default/grub.original
-
 # enable ctrl alt f1-f6 terminals
-sudo sed -i s/#[[:space:]]*GRUB_TERMINAL/GRUB_TERMINAL/ /etc/default/grub
-sudo update-grub
-
+if [ ! -z "$(sudo grep '# *GRUB_TERMINAL' /etc/default/grub)" ]; then
+	# backup original grub config (only the first time run)
+	sudo cp -n /etc/default/grub /etc/default/grub.original
+	sudo sed -i s/#[[:space:]]*GRUB_TERMINAL/GRUB_TERMINAL/ /etc/default/grub
+	sudo update-grub
+fi
 
 
 
 # install default software
 install gedit
 install vim-gnome
+install jedit
 install kdiff3
 
-install gnome-terminal
+install git-gui
+install $JAVA_PKG
+install python3
+
 install pal
 install par2
 install unrar
@@ -103,17 +145,13 @@ install ssh
 install tree
 install curl
 
-install git
-install git-gui
-install tortoisehg
-install $JAVA_PKG
-install python3
-
 install mplayer
 install xine-ui
 install vlc
 install kaffeine
 
+install gnome-terminal
+install numlockx
 install gparted
 install filelight
 install filezilla
@@ -123,39 +161,41 @@ install partimage
 install virtualbox-qt
 install android-tools-fastboot
 
-git config --global user.name "Jantje"
-git config --global user.email "nospam@example.com"
 
-# install drives
-sudo python3 fstab.py
+# turn on numlock automatically, is this line necessary? Needs testing
+# /usr/bin/numlockx on
 
 
 
-# install my own creations
-if [ -d ../home ]; then
-	echo "copying files in home"
-	cp -nr ../home/* ~ 
+# smoke tests
+if (! java -version 2>&1 | grep "version" | grep $JAVA_VERSION)
+then
+	echo "expecting java $JAVA_VERSION"
 fi
 
+
+
+
+# install my own scripts
 if [ ! -d ~/bin ]; then
 	mkdir ~/bin
 fi
 
 if [ -d bin ]; then
+	echo ""
 	echo "copying scripts in bin"
 	cp -nr bin/* ~/bin
 	sudo chmod +x ~/bin/*
 fi
 
-echo ""
+fetch_url $CHECKSTYLE_CACHE  $CHECKSTYLE_URL  $CHECKSTYLE_DESTINATION
+fetch_url $ANT_JUNIT_CACHE   $ANT_JUNIT_URL   $ANT_JUNIT_DESTINATION
+
+cd projects
+sh projects.sh
 
 
 
 
-# smoke tests
-if(! java -version 2>&1 | grep "version" | grep $JAVA_VERSION)
-then
-	echo "expecting java $JAVA_VERSION"
-fi
 
 
---
+++ projects/projects.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+
+git_dir=~/Development/git
+dependency_file="$start_dir/dependencies.txt"
+buildfile=~/Development/git/ant/master.xml
+
+
+if [ ! -d $git_dir ];then
+	mkdir $git_dir
+fi
+
+for repo in $(sed '/@EOF@/,$ d; s/=>.*//' repos.txt)
+do
+	repo_dir="$git_dir/$repo"
+	if [ -d $repo_dir ]
+	then
+		echo "repo already exists: $repo_dir"
+	else
+		git clone "https://bitbucket.org/Lievaart/$repo" $repo_dir
+	fi
+done
+
+echo "building ws"
+ant -f "$buildfile" -Dskip.test=true -Dskip.checkstyle=true -Dproject.name=ws master-deploy-jar -Dskip.resolve=true > /tmp/ant.log
+
+echo "building toolkit"
+ant -f "$buildfile" -Dskip.test=true -Dskip.checkstyle=true -Dproject.name=toolkit master-jar                       >> /tmp/ant.log
+
+echo "building antastic"
+ant -f "$buildfile" -Dskip.test=true -Dskip.checkstyle=true -Dproject.name=antastic master-deploy-jar               >> /tmp/ant.log
+
+echo "generating antastic metadata"
+ws generate
+
+echo "building remaining projects using antastic"
+antastic "$(pwd)/antastic.txt"
+
+
