3831263d27a6ed43aede33cd79fe9917d71ac895
me <me>
2018-09-06 14:41:06 +0200

    extracted root statements

---
+++ as-root.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+set -e                    # fail on errors
+sh -n install-as-root.sh          # check this file for syntax errors before executing it
+
+
+JAVA_PKG="openjdk-8-jdk"
+JAVA_VERSION="1.8"
+
+
+die() {
+    echo >&2 "Error: $@"
+    exit 1
+}
+
+check_status() {
+	if [ $1 -ne 0 ]
+	then
+		die "Installation aborted with status $1"
+	fi
+}
+
+update_once() {
+	if [ $UP_TO_DATE ]
+	then
+		set +e
+		sudo apt-get update
+		set -e
+		UP_TO_DATE=true
+	fi
+}
+
+install() {
+	if (!(dpkg -s $1 | grep "Status: install" )) > /dev/null
+	then
+		update_once
+		echo "Installing $1."
+		sudo apt-get -y install $1
+		check_status $?
+	else
+		echo "$1 is already installed"
+	fi
+}
+
+# enable firewall
+sudo ufw enable 
+echo ""
+
+if ! (dpkg -s $JAVA_PKG < /dev/null | grep "Status: install" )
+then
+	# remove older java versions
+	sudo apt-get remove -y openjdk* 
+	sudo apt-get autoremove
+	sudo apt-get autoclean
+	# add apt repository for openjdk
+	sudo add-apt-repository -y ppa:openjdk-r/ppa
+	update_once
+fi
+
+
+
+# allow users to invoke shutdown
+sudo chmod +s /sbin/shutdown
+
+
+# enable ctrl alt f1-f6 terminals
+if [ ! -z "$(sudo grep '# *GRUB_TERMINAL' /etc/default/grub)" ]; then
+	# backup original grub config (only the first time run)
+	sudo cp -n /etc/default/grub /etc/default/grub.original
+	sudo sed -i s/#[[:space:]]*GRUB_TERMINAL/GRUB_TERMINAL/ /etc/default/grub
+	sudo update-grub
+fi
+
+
+# install default software
+install gedit
+install vim-gnome
+install jedit
+install kdiff3
+
+install git
+install git-gui
+install $JAVA_PKG
+install python3
+install ant
+
+install pal
+install par2
+install unrar
+install ssh
+install tree
+install curl
+install jq
+
+install mplayer
+install xine-ui
+install vlc
+install kaffeine
+
+install gnome-terminal
+install numlockx
+install gparted
+install filelight
+install filezilla
+install sabnzbdplus
+install synaptic
+install partimage
+install virtualbox-qt
+install android-tools-fastboot
+
+
+# smoke tests
+if (! java -version 2>&1 | grep "version" | grep $JAVA_VERSION)
+then
+	echo "expecting java $JAVA_VERSION"
+fi
+
+
--- install.sh
+++ install.sh
@@ -8,9 +8,6 @@ LOG_DIR=/tmp/installer
 LOG_FILE=$LOG_DIR/installer_$(date +%y-%m-%d_%T).log
 UP_TO_DATE=false
 
-JAVA_PKG="openjdk-8-jdk"
-JAVA_VERSION="1.8"
-
 CHECKSTYLE_VERSION=8.11
 CHECKSTYLE_DESTINATION=~/Applications/checkstyle/checkstyle.jar
 CHECKSTYLE_CACHE="../cache/checkstyle/checkstyle-$CHECKSTYLE_VERSION.jar"
@@ -20,8 +17,6 @@ ANT_JUNIT_VERSION=1.8.4
 ANT_JUNIT_DESTINATION=~/.ant/lib/ant-junit.jar
 ANT_JUNIT_CACHE="../cache/ant-junit/ant-junit-$ANT_JUNIT_VERSION.jar"
 ANT_JUNIT_URL="https://repo1.maven.org/maven2/org/apache/ant/ant-junit/$ANT_JUNIT_VERSION/ant-junit-$ANT_JUNIT_VERSION.jar"
-
-
 
 
 die() {
@@ -38,35 +33,6 @@ log() {
 	echo $1 >> $LOG_FILE
 }
 
-check_status() {
-	if [ $1 -ne 0 ]
-	then
-		die "Installation aborted with status $1"
-	fi
-}
-
-update_once() {
-	if [ $UP_TO_DATE ]
-	then
-		set +e
-		sudo apt-get update
-		set -e
-		UP_TO_DATE=true
-	fi
-}
-
-install() {
-	if (!(dpkg -s $1 | grep "Status: install" )) > /dev/null
-	then
-		update_once
-		log "Installing $1."
-		sudo apt-get -y install $1
-		check_status $?
-	else
-		log "$1 is already installed"
-	fi
-}
-
 fetch_url() {
 	cache_file=$1
 	url=$2
@@ -80,10 +46,17 @@ fetch_url() {
 	fi
 }
 
+
+
+
 if [ "$HOME" = "/root" ]; then
-	echo "usage: sudo sh install.sh"
-	die "run script with root priviliges (sudo sh install.sh), not as root user"
+	die "do not run script as root!"
 fi
+
+if [ "$(id -u)" = "0" ]; then
+	die "do not run script as root!"
+fi
+
 
 if [ ! -f ~/.gitconfig ]
 then
@@ -91,42 +64,18 @@ then
 	read git_user
 	echo "enter git email"
 	read git_email
+fi
 
-	install git
+sudo sh as-root.sh
+
+if [ ! -f ~/.gitconfig ]
+then
 	git config --global user.name "$git_user"
 	git config --global user.email "$git_email"
 	git config --global credential.helper cache
 fi
 
-# enable firewall
-sudo ufw enable 
-echo ""
 
-
-if ! (dpkg -s $JAVA_PKG < /dev/null | grep "Status: install" )
-then
-	# remove older java versions
-	sudo apt-get remove -y openjdk* 
-	sudo apt-get autoremove
-	sudo apt-get autoclean
-	# add apt repository for openjdk
-	sudo add-apt-repository -y ppa:openjdk-r/ppa
-	update_once
-fi
-
-
-
-
-# allow users to invoke shutdown
-sudo chmod +s /sbin/shutdown
-
-# enable ctrl alt f1-f6 terminals
-if [ ! -z "$(sudo grep '# *GRUB_TERMINAL' /etc/default/grub)" ]; then
-	# backup original grub config (only the first time run)
-	sudo cp -n /etc/default/grub /etc/default/grub.original
-	sudo sed -i s/#[[:space:]]*GRUB_TERMINAL/GRUB_TERMINAL/ /etc/default/grub
-	sudo update-grub
-fi
 
 # register shortcuts for window snapping
 XFCE_SHORTCUTS_FILE=~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
@@ -154,40 +103,6 @@ fi
 
 
 
-# install default software
-install gedit
-install vim-gnome
-install jedit
-install kdiff3
-
-install git-gui
-install $JAVA_PKG
-install python3
-install ant
-
-install pal
-install par2
-install unrar
-install ssh
-install tree
-install curl
-install jq
-
-install mplayer
-install xine-ui
-install vlc
-install kaffeine
-
-install gnome-terminal
-install numlockx
-install gparted
-install filelight
-install filezilla
-install sabnzbdplus
-install synaptic
-install partimage
-install virtualbox-qt
-install android-tools-fastboot
 
 
 
@@ -212,8 +127,8 @@ log "installing eclipse"
 sh eclipse-minimal.sh >> $LOG_FILE
 
 
-# cd projects
-# sh projects.sh
+cd projects
+sh projects.sh
 
 
 
@@ -223,11 +138,6 @@ sh eclipse-minimal.sh >> $LOG_FILE
 
 
 
-# smoke tests
-if (! java -version 2>&1 | grep "version" | grep $JAVA_VERSION)
-then
-	echo "expecting java $JAVA_VERSION"
-fi
 
 
 
