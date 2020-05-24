18e4a6f032d0b573d48e58e26042a1a8c3203fc7
unknown <unknown>
2020-05-24 11:15:18 +0200

    new: auto link java api

--- projects/docgen.sh
+++ projects/docgen.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
 set -e
 
+preinstalled=/usr/share/doc
+target=~/Applications/doclauncher/api
+
+# link java api from preinstalled location
+for dir in $(ls ${preinstalled-} | grep jdk | grep doc)
+do
+	for api in $(find ${preinstalled-}/$dir -name api)
+	do
+		version=$(echo $dir | sed -r 's/[^0-9]+//g')
+		destination=$target/java$version
+		[ -e $destination ] || ln -s $api $destination
+	done
+done
+
+# generate javadoc from maven source
 root=~/Development/repo
 for file in $(find $root | grep 'src.jar' | sort)
 do
 	name=$(echo "$file" | sed -r 's:.*/::;s:(-[0-9.]+)?-src.jar$::')
 	src="/tmp/spool/src/$name"
-	destination=~/Applications/doclauncher/api/$name
+	destination=$target/$name
 	if [ -e $destination ]
 	then
 		continue
 	fi
 	if [ ! -e "$destination" -a "$(unzip -l $file | grep '.java$')" != "" ]
 	then
-		echo "bla $file"
+		echo "$file"
 		mkdir -p $src
 		unzip -n $file -d $src
 		pwd
 		ant -f javadoc.xml -Dlib=$name
 	fi
 done
+
+
+
