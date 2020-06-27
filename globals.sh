be74bae9f5594db735e2385d2d1e4f6ac9f0eb9e
nospam <nospam>
2020-06-27 11:36:14 +0200

    arch fixes

--- globals.sh
+++ globals.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # global properties
-GIT_DIR=~/Development/git
-INSTALLER_DIR=${GIT_DIR?}/installer
-IBIN_DIR=${INSTALLER_DIR?}/ibin
 HOME=~
 if [ "$HOME" = "/root" ]; then
 	HOME=/home/${SUDO_USER:?}
 fi
 
+GIT_DIR=$HOME/Development/git
+INSTALLER_DIR=${GIT_DIR?}/installer
+IBIN_DIR=${INSTALLER_DIR?}/ibin
 
 
