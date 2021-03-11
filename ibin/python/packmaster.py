d3bd50ff3f98e71b76f74572ce8b43a720245575
nospam <nospam>
2021-03-11 16:03:18 +0100

    new: packmaster supports more formats

--- ibin/python/packmaster.py
+++ ibin/python/packmaster.py
@@ -6,59 +6,26 @@ import subprocess
 from pathlib import Path
 
 
-
-
-root = os.getcwd()
-root_path = Path(root)
-print("running packmaster in: " + root)
-
-
-def unpack(glob, command, flag):
-	z7 = root_path.glob(glob)
-	for zip_file in z7:
-		unpack_dir = zip_file.parent.as_posix() + '/' + zip_file.stem
+def unpack(root, glob, args):
+	for archive in Path(root).glob(glob):
+		if os.path.isdir(archive):
+			continue
+		parent_dir = archive.parent.as_posix()
+		os.chdir(parent_dir)
+		unpack_dir = parent_dir + '/' + archive.stem
 		if Path(unpack_dir).exists():
 			shutil.rmtree(unpack_dir)
 		os.mkdir(unpack_dir)
-		print('\tcopying ' + zip_file.stem + ' to ' + unpack_dir)
-		shutil.copyfile(zip_file.as_posix(), unpack_dir + '/' + zip_file.name)
-		os.chdir(unpack_dir)
-		print('unpacking ' + zip_file.stem)
-		subprocess.check_output([command, flag, zip_file.name])
-	
-
-unpack('**/*.7z', 'p7zip', '-d')
+		print('unpacking ' + archive.name)
+		subprocess.check_output(args(archive.name, unpack_dir))
 
 
-rar = root_path.glob('**/*.rar')
-for rar_file in rar:
-	parent_dir = rar_file.parent.as_posix()
-	unpack_dir = parent_dir + '/' + rar_file.stem
-	if Path(unpack_dir).exists():
-		shutil.rmtree(unpack_dir)
-	os.chdir(parent_dir)
-	print('unpacking ' + rar_file.stem)
-	subprocess.check_output(['unrar', 'x', '-ad', rar_file.name])
-
-zip_glob = root_path.glob('**/*.zip')
-for zip_file in zip_glob:
-	parent_dir = zip_file.parent.as_posix()
-	unpack_dir = parent_dir + '/' + zip_file.stem
-	if Path(unpack_dir).exists():
-		shutil.rmtree(unpack_dir)
-	os.mkdir(unpack_dir)
-	tmp_file = unpack_dir + '/' + zip_file.name
-	shutil.copyfile(zip_file.as_posix(), tmp_file)
-	os.chdir(unpack_dir)
-	print('unpacking ' + zip_file.stem)
-	subprocess.check_output(['unzip', zip_file.name])
-	os.remove(tmp_file)
-
-
-
-
-# find 7z, zip, rar
-# create directory per file
-# unpack 7z, zip, rar
+root=os.getcwd()
+print("running packmaster in: " + root)
+unpack(root, '**/*.zip',    lambda archive, unpack: ['unzip', archive, '-d', unpack])
+unpack(root, '**/*.rar',    lambda archive, unpack: ['unrar', 'x', '-ad', archive])
+unpack(root, '**/*.tar',    lambda archive, unpack: ['tar', '-xf',  archive, '-C', unpack])
+unpack(root, '**/*.tar.gz', lambda archive, unpack: ['tar', '-zxf', archive, '-C', unpack])
+unpack(root, '**/*.7z',     lambda archive, unpack: ['7z', 'x', archive , '-o' + unpack])
 
 
--- link/home/.vimrc
+++ link/home/.vimrc
@@ -7,6 +7,7 @@ let g:airline#extensions#tabline#enabled = 1
 let g:airline_statusline_ontop=0
 let g:airline_theme='powerlineish'
 
+" use syntax highlighting on search
 syntax on
 
 " don't highlight matching parenthesis
@@ -30,21 +31,14 @@ set shiftwidth=4
 set tabstop=4
 
 
-" copy and paste (shift+insert)
-map <S-Insert> <MiddleMouse>
-map! <S-Insert> <MiddleMouse>
-
-" ctrl-n to toggle line numbers
-nmap <C-N> :set invnumber<CR>
-
+" delete first character on line and move down
+map <F2> @='^xj'<CR>
+" comment line with '#' and move down
+nmap <F3> @='I#<C-V><Esc>j'<CR>
 " delete first character on line and move down
 imap <F2> <Esc>@='^xj'<CR>i
 " comment line with '#' and move down
 imap <F3> <Esc>@='I#<C-V><Esc>j'<CR>i
-" delete first character on line and move down
-nmap <F2> @='^xj'<CR>
-" comment line with '#' and move down
-nmap <F3> @='I#<C-V><Esc>j'<CR>
 
 " on F5 store last executed command in register 'r'
 map <F5> :let @r = @:<CR>
@@ -58,6 +52,9 @@ map! <F8> <ESC>:w <bar> !clear; ./% <CR>
 
 map <F9> :NERDTreeToggle <CR>
 map! <F9> <ESC>:NERDTreeToggle <CR>
+
+" ctrl-n to toggle line numbers
+nmap <C-N> :set invnumber<CR>
 
 " ctrl-alt arrow to move line
 nnoremap <C-A-Up>          :m .-2  <CR>
@@ -73,34 +70,20 @@ nnoremap <C-J> :wincmd k <CR>:resize -1 <CR>
 nnoremap <C-K> :wincmd k <CR>:resize +1 <CR>
 nnoremap <C-L> :wincmd h <CR>:vertical resize +1 <CR>
 
+" navigating buffers
+nnoremap + :bn<CR>
+nnoremap - :bp<CR>
+nnoremap _ :bd<CR>
+
 nnoremap ;c :close   <CR>
 nnoremap ;h :split   <CR>
-nnoremap ;q :quitall <CR>
 nnoremap ;v :vsplit  <CR>
-nnoremap ;w :w       <CR>
-nnoremap ;x :x       <CR>
-
-nnoremap ;1 :b 1 <CR>
-nnoremap ;2 :b 2 <CR>
-nnoremap ;3 :b 3 <CR>
-nnoremap ;4 :b 4 <CR>
-nnoremap ;5 :b 5 <CR>
-nnoremap ;6 :b 6 <CR>
-nnoremap ;7 :b 7 <CR>
-nnoremap ;8 :b 8 <CR>
-nnoremap ;9 :b 9 <CR>
-nnoremap ;0 :b 0 <CR>
 
 nnoremap ;$ :%s/$\(\w\+\)/${\1:?}/gc<CR>
 nnoremap ;; :nohlsearch <CR>
 nnoremap ;. :cnext      <CR>
 nnoremap ;, :clast      <CR>
 nnoremap ;/ :cope       <CR>
-
-" navigating buffers
-nnoremap + :bn<CR>
-nnoremap - :bp<CR>
-nnoremap _ :bd<CR>
 
 
 
